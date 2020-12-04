3f04a58626f3443a8ce79e82691495fdcf26d6b5
Angad Kambli <angad-k@users.noreply.github.com>
2020-12-04 21:28:19 +0530

    setup project

---
+++ CUDA 11.0 Runtime1/Driver.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include "Driver.h"
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+
+}
